981bfc901f71194c3ba57ec8b37c6e4ea36e1ba3
anmol-rathi <anmolrathi0804@gmail.com>
2026-07-20 11:21:45 +0530

    Time: 523 ms (37.36%) | Memory: 0B (100.00%) - LeetSync

---
+++ 197-rising-temperature/rising-temperature.sql
@@ -0,0 +1,6 @@
+# Write your MySQL query statement below
+select today.id
+from Weather as yesterday
+join weather as today
+where datediff(today.recordDate,yesterday.recordDate)=1
+    and today.temperature> yesterday.temperature
